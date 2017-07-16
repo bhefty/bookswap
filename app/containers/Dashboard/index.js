8c61a6ad00e5c2f134ffea975f8b2d504b0aca80
Bill Hefty <billhefty@gmail.com>
2017-07-16 18:55:48 -0500

    check for authentication

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -7,25 +7,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { loginRequest } from 'auth/actions'
+import { makeSelectIdToken } from 'auth/selectors'
 
 export class Dashboard extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  componentWillMount = () => {
+    if (!this.props.isLoggedIn) {
+      this.props.login()
+    }
+  }
+
   render () {
     return (
       <div>
-        <h2>Hello from Dashboard!</h2>
+        {this.props.isLoggedIn ? // eslint-disable-line operator-linebreak
+          <div>
+            <h2>Hello from Dashboard!</h2>
+          </div>
+          : // eslint-disable-line operator-linebreak
+          <div>
+            <h2>You are not logged in yet</h2>
+          </div>
+        }
       </div>
     )
   }
 }
 
 Dashboard.PropTypes = {
-  dispatch: PropTypes.func.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  login: PropTypes.func.isRequired
 }
+
+const mapStateToProps = createStructuredSelector({
+  isLoggedIn: makeSelectIdToken()
+})
 
 function mapDispatchToProps (dispatch) {
   return {
-    dispatch
+    login: () => dispatch(loginRequest())
   }
 }
 
-export default connect(null, mapDispatchToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
