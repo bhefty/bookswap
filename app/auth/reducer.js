8e67c1a2bef8784c54e7ba29b5d025961078c831
Bill Hefty <billhefty@gmail.com>
2017-07-16 11:38:06 -0500

    add auth reducer

---
+++ app/auth/reducer.js
@@ -0,0 +1,40 @@
+import { fromJS } from 'immutable'
+
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT
+} from './constants'
+
+const initialState = fromJS({
+  isLoggingIn: false,
+  idToken: null,
+  profile: null,
+  error: null
+})
+
+function authReducer (state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return state
+        .set('isLoggingIn', true)
+    case LOGIN_SUCCESS:
+      return state
+        .set('isLoggingIn', false)
+        .set('idToken', action.idToken)
+        .set('profile', action.profile)
+    case LOGIN_FAILURE:
+      return state
+        .set('isLogginIn', false)
+        .set('idToken', null)
+        .set('profile', null)
+        .set('error', action.error)
+    case LOGOUT:
+      return initialState
+    default:
+      return state
+  }
+}
+
+export default authReducer
