63353a5f52cf1f79544ecb98b19c840af4600752
Bill Hefty <billhefty@gmail.com>
2017-07-16 11:21:15 -0500

    add util to set/remove/get auth state

---
+++ app/utils/auth.js
@@ -0,0 +1,36 @@
+// import { AuthState } from 'auth/reducer'
+
+let localStorage
+
+// If testing, use local storage polyfill
+if (global.process && process.env.NODE_ENV === 'test') {
+  localStorage = require('localStorage')
+} else {
+  // If not use the browser one
+  localStorage = global.window.localStorage
+}
+
+export const ID_TOKEN = 'id_token'
+export const PROFILE = 'profile'
+
+export const setStoredAuthState = (profile, idToken) => {
+  localStorage.setItem(ID_TOKEN, idToken)
+  localStorage.setItem(PROFILE, JSON.stsringify(profile))
+}
+
+export const removeStoredAuthState = () => {
+  localStorage.removeItem(ID_TOKEN)
+  localStorage.removeItem(PROFILE)
+}
+
+export const getStoredAuthState = () => {
+  try {
+    const idToken = localStorage.getItem(ID_TOKEN)
+    const profile = JSON.parse(localStorage.getItem(PROFILE))
+
+    return { idToken, profile }
+  } catch (err) {
+    removeStoredAuthState()
+    return {}
+  }
+}
