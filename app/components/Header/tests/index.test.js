4ce512d7207bfbb04caf5223e889725d4cdba294
Bill Hefty <billhefty@gmail.com>
2017-07-16 21:18:44 -0500

    test for conditional button renders based on this.props.profile

--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -10,4 +10,33 @@ describe('<Header />', () => {
     )
     expect(renderedComponent.find('header').length).toEqual(1)
   })
+
+  it('should render Dashboard link if logged in', () => {
+    const profile = {
+      email: 'joeschmoe@example.com',
+      name: 'Joe Schmoe'
+    }
+    const renderedComponent = shallow(
+      <Header profile={profile} />
+    )
+    expect(renderedComponent.contains('Dashboard')).toBe(true)
+  })
+
+  it('should render Logout button if logged in', () => {
+    const profile = {
+      email: 'joeschmoe@example.com',
+      name: 'Joe Schmoe'
+    }
+    const renderedComponent = shallow(
+      <Header profile={profile} />
+    )
+    expect(renderedComponent.contains('Logout')).toBe(true)
+  })
+
+  it('should render Login button if not logged in', () => {
+    const renderedComponent = shallow(
+      <Header />
+    )
+    expect(renderedComponent.contains('Login')).toBe(true)
+  })
 })
