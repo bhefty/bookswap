e80a5a7166fbbbf6d255c4f3a9494b1ddd920da2
Bill Hefty <billhefty@gmail.com>
2017-07-19 23:03:14 -0500

    add example text

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -11,6 +11,7 @@ export class Dashboard extends React.PureComponent { // eslint-disable-line reac
     return (
       <div>
         <h2>Hello from Dashboard!</h2>
+        <p>This is a private page that only authorized users can view. Check out <code>app/routes.js</code> to see how this is done.</p>
       </div>
     )
   }
