4b81cafab93ca96c2d1b1037eda8e8394855d7e4
Bill Hefty <billhefty@gmail.com>
2017-08-05 15:05:15 -0500

    add cover to background

--- app/components/Splash/Background.js
+++ app/components/Splash/Background.js
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export default styled.div`
   background: #fff url('${props => props.backgroundImg}') no-repeat center center;
+  background-size: cover;
   height: 400px;
 `
