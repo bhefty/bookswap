2a9cde34cec26f30b76b5f8d90bf9595f83f9f71
Bill Hefty <billhefty@gmail.com>
2017-08-05 14:57:04 -0500

    adjust styling to center splash text

--- app/components/Splash/Background.js
+++ app/components/Splash/Background.js
@@ -2,5 +2,5 @@ import styled from 'styled-components'
 
 export default styled.div`
   background: #fff url('${props => props.backgroundImg}') no-repeat center center;
-  max-height: 400px;
+  height: 400px;
 `
--- app/components/Splash/TextWrapper.js
+++ app/components/Splash/TextWrapper.js
@@ -1,6 +1,22 @@
 import styled from 'styled-components'
 
 export default styled.div`
+  position: relative;
+  top: 85px;
   background: rgba(46, 52, 64, 0.7);
   height: 230px;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: row wrap;
+
+  h1 {
+    font-size: 4em;
+  }
+
+  p {
+    margin: 0;
+  }
 `
