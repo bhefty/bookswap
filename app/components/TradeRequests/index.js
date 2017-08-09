79ca015f47322822e3438e25842174cd93f94b30
Bill Hefty <billhefty@gmail.com>
2017-08-08 19:43:50 -0500

    render requests base on props

--- app/components/TradeRequests/index.js
+++ app/components/TradeRequests/index.js
@@ -5,35 +5,39 @@
 */
 
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 import UL from './UL'
 import RequestItem from './RequestItem'
 
-function TradeRequests () {
+const mapRequests = (trades) => {
+  return trades.map((trade, idx) => {
+    return <RequestItem key={idx} title={trade.title} />
+  })
+}
+
+function TradeRequests (props) {
+  const { myApproval, othersApproval } = props
   return (
     <div>
       <h2>Trade Requests</h2>
       <p>Pending my approval:</p>
-      <UL>
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-      </UL>
+      { myApproval.length < 1
+        ? <p>No pending requests for your approval.</p>
+        : <UL>{ mapRequests(myApproval) }</UL>
+      }
       <p>Pending other's approval:</p>
-      <UL>
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-        <RequestItem title='Title' />
-      </UL>
+      { othersApproval.length < 1
+        ? <p>You have no pending trades.</p>
+        : <UL>{ mapRequests(othersApproval) }</UL>
+      }
     </div>
   )
 }
 
-// TradeRequests.propTypes = {
-
-// }
+TradeRequests.propTypes = {
+  myApproval: PropTypes.array,
+  othersApproval: PropTypes.array
+}
 
 export default TradeRequests
