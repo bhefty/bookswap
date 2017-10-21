7bceaa9e47e2db85447a3c30baaa195d815e3233
bhefty <billhefty@gmail.com>
2017-10-20 19:38:36 -0500

    return json response and remove console logs

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 
 // GET /user/:id to retrieve user profile
 router.get('/:id', (req, res) => {
-  User.find({ userId: req.params.id }, (err, profile) => {
+  User.find({ user_id: req.params.id }, (err, profile) => {
     if (err) res.send(err)
     res.json(profile)
   })
@@ -12,13 +12,13 @@ router.get('/:id', (req, res) => {
 
 // PUT /user/register to register a new user
 router.put('/register', (req, res) => {
-  const { userId, name, picture } = req.body
+  const { user_id, name, picture } = req.body
   // Verify user does not already exist
-  User.findOne({ userId: userId }, (err, existingUser) => {
+  User.findOne({ user_id: user_id }, (err, existingUser) => {
     if (err) res.send(err)
     if (!existingUser) {
       const registerNewUser = new User({
-        userId,
+        user_id,
         name,
         picture
       })
@@ -26,13 +26,11 @@ router.put('/register', (req, res) => {
         if (err) {
           res.send(err)
         } else {
-          console.log('New user registered!')
-          res.send('Created!')
+          res.json({ msg: 'New user created successfully' })
         }
       })
     } else {
-      console.log('already exists')
-      res.send('nice try buddy')
+      res.json({ msg: 'User already exists' })
     }
   })
 })
